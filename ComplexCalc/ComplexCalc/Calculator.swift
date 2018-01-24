3a69f7d8b2b07f7b7ff57e33cebd07661c041199
Jerry Li <ruiheli@uw.edu>
2018-01-23 18:15:15 -0800

    all tests passed

--- ComplexCalc/ComplexCalc/Calculator.swift
+++ ComplexCalc/ComplexCalc/Calculator.swift
@@ -13,16 +13,56 @@ class Calculator {
     func add(lhs: Int, rhs: Int) -> Int {
         return lhs + rhs
     }
+    
+    func add(_ arr:[Int]) -> Int{
+        return arr.reduce(0, {$0 + $1})
+    }
+    
+    func add(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int){
+        return (lhs.0 + rhs.0, lhs.1 + rhs.1)
+    }
+    
+    func add(lhs: [String: Int], rhs: [String: Int]) -> [String: Int]{
+        return ["x": lhs["x"]! + rhs["x"]!, "y": lhs["y"]! + rhs["y"]!]
+    }
+    
     func subtract(lhs: Int, rhs: Int) -> Int {
         return lhs - rhs
     }
+    
+    func subtract(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int){
+        return (lhs.0 - rhs.0, lhs.1 - rhs.1)
+    }
+    
+    func subtract(lhs: [String: Int], rhs: [String: Int]) -> [String: Int]{
+        return ["x": lhs["x"]! - rhs["x"]!, "y": lhs["y"]! - rhs["y"]!]
+    }
+    
     func multiply(lhs: Int, rhs: Int) -> Int {
         return lhs * rhs
     }
+    
+    func multiply(_ arr:[Int]) -> Int{
+        return arr.reduce(1, {$0 * $1})
+    }
+    
     func divide(lhs: Int, rhs: Int) -> Int {
         return lhs/rhs
     }
-    func mathOp(lhs: Int, rhs: Int, op: (Int, Int) -> Int) -> Int {
+    
+    func mathOp(lhs: Int, rhs: Int, op: (_:Int, _:Int) -> Int) -> Int {
         return op(lhs,rhs)
+    }
+    
+    func mathOp(args: [Int], beg: Int, op: (_:Int, _:Int) -> Int) -> Int{
+        return args.reduce(beg, op)
+    }
+    
+    func count(_ arr:[Int]) -> Int {
+        return arr.count
+    }
+    
+    func avg(_ arr: [Int]) -> Int {
+        return arr.count == 0 ? 0 : self.add(arr)/arr.count
     }
 }
